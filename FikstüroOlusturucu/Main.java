0e77cf8d9ea8cebfa89413ea06deee184132f6d7
Ömer <117285777+rose-omer@users.noreply.github.com>
2024-08-14 17:48:44 +0300

    Add files via upload

---
+++ FikstüroOlusturucu/Main.java
@@ -0,0 +1,30 @@
+package FikstüroOlusturucu;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        FixtureGenerator<String> fixtureGenerator = new FixtureGenerator();
+
+        List<String> teams = new LinkedList<>();
+        teams.add("Galatasaray");
+        teams.add("Bursaspor");
+        teams.add("Fenerbahçe");
+        teams.add("Beşiktaş");
+        teams.add("Başakşehir");
+        teams.add("Trabzonspor");
+
+        List<List<Fixture<String>>> rounds = fixtureGenerator.getFixtures(teams, true);
+        for(int i=0; i<rounds.size(); i++){
+            System.out.println("Round " + (i+1));
+            List<Fixture<String>> round = rounds.get(i);
+            for(Fixture<String> fixture: round){
+                System.out.println(fixture.getHomeTeam() + " vs " + fixture.getAwayTeam());
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
